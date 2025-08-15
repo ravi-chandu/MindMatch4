36df8adb429952e377dc31f92d51fb241bad9aad
Ravi chandu <92777166+ravi-chandu@users.noreply.github.com>
2025-08-15 13:39:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MindMatch4/", 
-  build: {
-    outDir: "dist",
-    sourcemap: false
-  }
+  base: "/MindMatch4/",   
+  build: { outDir: "dist", sourcemap: false }
 });
